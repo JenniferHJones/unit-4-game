c04d1b4a316329db09a74af6cf8cc7d3ccba49be
JJ <jenjones@ucdavis.edu>
2019-01-11 17:21:43 -0800

    index and js files

---
+++ assets/javascript/game.js
@@ -0,0 +1,47 @@
+// variables
+var randomNumber = [];
+var win = 0;
+var loss = 0;
+var counter = [];
+var crystal1Number = [];
+var crystal2Number = [];
+var crystal3Number = [];
+var crystal4Number = [];
+
+// computer displays random number between 19 - 120
+var numberDisplay = Math.floor((Math.random() * 102) + 19);
+randomNumber.push(numberDisplay);
+console.log(randomNumber[0]);
+
+// computer assigns random number between 1 - 12 to crystal1 
+var randomCrystal1 = Math.floor((Math.random() * 12) + 1);
+crystal1Number.push(randomCrystal1);
+console.log(crystal1Number);
+
+// computer assigns random number between 1 - 12 to crystal2 
+var randomCrystal2 = Math.floor((Math.random() * 12) + 1);
+crystal2Number.push(randomCrystal2);
+console.log(crystal2Number);
+
+// computer assigns random number between 1 - 12 to crystal3 
+var randomCrystal3 = Math.floor((Math.random() * 12) + 1);
+crystal3Number.push(randomCrystal3);
+console.log(crystal3Number);
+
+// computer assigns random number between 1 - 12 to crystal4 
+var randomCrystal4 = Math.floor((Math.random() * 12) + 1);
+crystal4Number.push(randomCrystal4);
+console.log(crystal4Number);
+
+// on click function for crystal1
+$("#crystal1").on("click", function () {
+    console.log("yes!");
+});
+
+// function to add clicked crystal values to counter
+document.onkeydown = function (event) {
+    var crystalSelect = event.key;
+    counter.push(crystalSelect);
+};
+
+
